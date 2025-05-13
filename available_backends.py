c6a14a2acb7b3d418db91f3c02e385b4afd637a2
Braja Gopal Das <brajad10@gmail.com>
2025-05-13 13:24:37 +0530

    Asura

--- available_backends.py
+++
@@ -1,7 +0,0 @@
-from qiskit_ibm_provider import IBMProvider
-
-# Load your saved IBM Quantum account
-provider = IBMProvider()
-
-# List available backends
-print(provider.backends())
--- qiskit_aer_test.py
+++
@@ -1,9 +0,0 @@
-try:
-    from qiskit_aer import Aer
-    print("Qiskit Aer is installed.")
-except ImportError:
-    print("Qiskit Aer is NOT installed.")
-
-from qiskit_aer import Aer
-print(Aer.backends())
-
